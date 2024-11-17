f56e0f076623ce534b475d78cadeaeaca0e5f93a
santicounago04 <santicounago.ort@gmail.com>
2024-11-16 23:56:04 -0300

    added styles

--- obligatorio-02/src/App.jsx
+++ obligatorio-02/src/App.jsx
@@ -1,7 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import { baseURL, apiKey } from "./helpers/constants";
 
 function App() {
+  // Usamos useEffect para hacer la llamada solo una vez cuando el componente se monta
+  useEffect(() => {
+    fetch(`${baseURL}/events.json?classificationName=music&apikey=${apiKey}`)
+      .then((response) => response.json()) // Aquí debes invocar json() con los paréntesis
+      .then((data) => {
+        console.log(data); // Ahora 'data' será un objeto con la respuesta de la API
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error); // En caso de que haya un error en la petición
+      });
+  }, []); // El segundo argumento vacío [] asegura que la llamada solo se haga una vez
+
+  useEffect(() => {
+    fetch(
+      `https://us-west-1.geolocated.io/ip/167.57.148.113?api-key=Pe37sV0kbJ8WLpe.P2kVH2HLAjLt229dfrT2At5r9R1lKpqE`
+    )
+      .then((response) => response.json()) // Aquí debes invocar json() con los paréntesis
+      .then((data) => {
+        console.log(data); // Ahora 'data' será un objeto con la respuesta de la API
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error); // En caso de que haya un error en la petición
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://api.ipify.org?format=json`)
+      .then((response) => response.json()) // Aquí debes invocar json() con los paréntesis
+      .then((data) => {
+        console.log(data); // Ahora 'data' será un objeto con la respuesta de la API
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error); // En caso de que haya un error en la petición
+      });
+  }, []);
   return <></>;
 }
 
---
+++ obligatorio-02/src/helpers/constants.js
@@ -0,0 +1,2 @@
+export const baseURL = "https://app.ticketmaster.com/discovery/v2";
+export const apiKey = "HEAnEYrmoCIFjYIUjQGykOwzAbkYmNuG";
